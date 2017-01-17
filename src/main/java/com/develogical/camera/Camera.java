4eb40ddaf4832c26080bf5394ad4d02929582dea
Bobby Raju <bobby.raju@swisscom.com>
2017-01-17 17:11:02 +0000

    Mock objects lab

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -1,17 +1,46 @@
 package com.develogical.camera;
 
-public class Camera {
+public class Camera implements WriteListener{
+    Sensor sensor;
+    MemoryCard mc;
+    Boolean power, mcISWriting;
+
+    public Camera(Sensor sensor, MemoryCard mc) {
+        this.sensor = sensor;
+        this.mc = mc;
+        this.power = false;
+        this.mcISWriting = false;
+    }
+
+    public Camera(Sensor sensor) {
+        this.sensor = sensor;
+        this.power = false;
+        this.mcISWriting = false;
+    }
 
     public void pressShutter() {
-        // not implemented
+        if (power) {
+            mcISWriting = true;
+            mc.write(sensor.readData());
+        }
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
+        power = true;
     }
 
     public void powerOff() {
-       // not implemented
+        if(!mcISWriting) {
+            sensor.powerDown();
+            power = false;
+        }
+    }
+
+    @Override
+    public void writeComplete() {
+        mcISWriting = false;
+        sensor.powerDown();
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,5 +1,6 @@
 package com.develogical.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,7 +13,100 @@ public class CameraTest {
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
 
-         // write your test here
+         Sensor sensor = context.mock(Sensor.class);
+
+         context.checking(new Expectations(){{
+             exactly(1).of(sensor).powerUp();
+         }});
+
+         new Camera(sensor).powerOn();
     }
 
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        Sensor sensor = context.mock(Sensor.class);
+
+        context.checking(new Expectations(){{
+            exactly(1).of(sensor).powerDown();
+        }});
+
+        new Camera(sensor).powerOff();
+    }
+
+    @Test
+    public void shutterWithPowerOn() {
+        Sensor sensor = context.mock(Sensor.class);
+        MemoryCard mc = context.mock(MemoryCard.class);
+        byte[] data = new byte[] {123};
+
+        context.checking(new Expectations(){{
+            exactly(1).of(sensor).powerUp();
+            exactly(1).of(sensor).readData();
+                will (returnValue(data));
+            exactly(1).of(mc).write(data);
+        }});
+
+        Camera camera = new Camera(sensor, mc);
+        camera.powerOn();
+        camera.pressShutter();
+    }
+    
+    @Test
+    public void pressingTheShutterWhenThePowerIsOffDoesNothing() {
+        Sensor sensor = context.mock(Sensor.class);
+        MemoryCard mc = context.mock(MemoryCard.class);
+        byte[] data = new byte[] {123};
+
+        context.checking(new Expectations(){{
+            allowing(sensor).powerDown();
+            exactly(0).of(sensor).readData();
+            will (returnValue(data));
+            exactly(0).of(mc).write(data);
+        }});
+
+        Camera camera = new Camera(sensor, mc);
+        camera.powerOff();
+        camera.pressShutter();
+    }
+
+    @Test
+    public void powerOffWhenDataWrite() {
+        Sensor sensor = context.mock(Sensor.class);
+        MemoryCard mc = context.mock(MemoryCard.class);
+        byte[] data = new byte[] {123};
+
+        Camera camera = new Camera(sensor, mc);
+        context.checking(new Expectations(){{
+            exactly(1).of(sensor).powerUp();
+            exactly(1).of(sensor).readData();
+            will (returnValue(data));
+            exactly(1).of(mc).write(data);
+            exactly(0).of(sensor).powerDown();
+        }});
+
+        camera.powerOn();
+        camera.pressShutter();
+        camera.powerOff();
+    }
+
+    @Test
+    public void writeCompleteThanPowerDown() {
+        Sensor sensor = context.mock(Sensor.class);
+        MemoryCard mc = context.mock(MemoryCard.class);
+        byte[] data = new byte[] {123};
+
+        Camera camera = new Camera(sensor, mc);
+        context.checking(new Expectations(){{
+            allowing(sensor).powerUp();
+            exactly(1).of(sensor).readData();
+            will (returnValue(data));
+            exactly(1).of(mc).write(data);
+            exactly(1).of(sensor).powerDown();
+        }});
+
+        camera.powerOn();
+        camera.pressShutter();
+        camera.writeComplete();
+    }
 }
+
